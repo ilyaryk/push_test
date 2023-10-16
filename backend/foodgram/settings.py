2d30343ddf9749c23a58f11d074114fde0717c84
Илья Рыкованов <ilya.rykovanov@gmail.com>
2023-10-16 18:13:52 +0700

    1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 print(BASE_DIR)
 
 SECRET_KEY = os.getenv('SECRET_KEY',
@@ -62,7 +62,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
@@ -71,10 +71,10 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
         'HOST': 'localhost',
         'PORT': '5432',
     }
-}'''
+}
 
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
@@ -83,7 +83,7 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='5432')
     }
-}
+}'''
 
 
 AUTH_PASSWORD_VALIDATORS = [
